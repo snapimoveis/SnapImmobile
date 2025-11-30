cac84cc98af54fb7a185c268d433ae3a368b34b5
snapimoveis <snapimoveis@gmail.com>
2025-11-30 01:37:20 +0000

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// A LINHA ABAIXO É OBRIGATÓRIA PARA O DESIGN FUNCIONAR
-import './index.css'; 
+import './index.css'; // <--- ESTA LINHA É OBRIGATÓRIA
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
